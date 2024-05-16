3878b4fb2b2e2f15b155c9f3a800276ca644081e
Aditya Sharma <105538453+Addy3126@users.noreply.github.com>
2024-05-16 19:53:21 +0530

    Colour theme

--- app/main.py
+++ app/main.py
@@ -11,30 +11,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# # Gets all the latest URL's from the NY Times Technology Section. (see news_extract.py for more detail)
-# content_string = get_content_string(my_url)
-# starts, ends = find_occurrences(content_string)
-# url_list = get_all_urls(starts, ends, content_string)
-#
-# # Gets the article summary and performs sentiment analysis on the chosen URL.
-# # For more information on how this works, visit news_scrape.py and news_nlp.py!
-# for url in url_list:
-#     try:
-#         print("Article URL: " + str(url))
-#         article_summary = summarize_article(url)
-#         find_sentiment(article_summary)
-#         print("------------------------------------------------")
-#         time.sleep(7)  # Allows user to get through all the text.
-#     except ArticleException:
-#         print(f"Failed to download the article from {url}")
-#         print("------------------------------------------------")
-#
-# # Closing Messages
-# print()
-# print("The articles have been successfully extracted!")
-# print("In total, we were able to extract " + str(len(url_list)) + " different articles!")
-# print("Thanks for participating, " + "!")
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
